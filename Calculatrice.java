e549a483305b56d43da2a6af5593ffda088c8719
Yong Jiang Liu <6189328@crosemont.qc.ca>
2022-07-05 17:24:40 -0400

    1. Fixed retour button with dispose(); 2. Not open new window;

--- Calculatrice.java
+++ Calculatrice.java
@@ -11,6 +11,7 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -129,7 +130,8 @@ public class Calculatrice extends JFrame implements ActionListener{
             a = 0;  b = 0; result = 0;
         }
         if (e.getSource() == btnRetour){
-            MyMenu menu = new MyMenu();
+            //MyMenu menu = new MyMenu();
+            dispose();
         }
     }
 }
